bfa8c923f5eb899882a14cbf7f483cab08b0ba71
M24W0590XIAZHENGWEI <775843700@qq.com>
2025-07-01 11:37:58 +0900

    Add files via upload

---
+++ homework8.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+def dft_matrix(N):
+    '''
+    Create a DFT transform matrix, W, of size N.
+    
+    @param:
+    N (scalar): number of columns in the transform matrix
+    
+    @result:
+    W (NxN array): a matrix of dtype='complex' whose (k,n)^th element is:
+           W[k,n] = cos(2*np.pi*k*n/N) - j*sin(2*np.pi*k*n/N)
+                 = exp(-j*2πkn/N)
+    '''
+    k = np.arange(N).reshape((N, 1))  # column vector
+    n = np.arange(N).reshape((1, N))  # row vector
+    W = np.exp(-2j * np.pi * k * n / N)
+    return W
